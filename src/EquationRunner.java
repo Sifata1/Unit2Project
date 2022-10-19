fc7f960a4cb505bdc079a15326a562cd3cf23fd9
sifatanan <sanan6576@bths.edu>
2022-10-18 20:36:59 -0400

    Finished Coding

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class EquationRunner {
     public static void main(String[] args)
     {
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,5 +1,3 @@
-import java.text.DecimalFormat;
-
 import static java.lang.Math.*;
 
 public class LinearEquation {
