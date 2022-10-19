58116c99e4b8d6ba758843bff4e15dd9a254803c
sifatanan <sanan6576@bths.edu>
2022-10-19 18:44:28 -0400

    Finished Coding

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,16 +1,19 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class EquationRunner {
     public static void main(String[] args)
     {
+        DecimalFormat df = new DecimalFormat("0.00"); // This is used to round to 2 decimal place values.
 
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the first x-y coordinate point:");
+        System.out.println("Enter the first x-y coordinate point:"); // User is asked for two coordinate points.
         String firstPair = s.nextLine();
         System.out.println("Enter the second x-y coordinate point:");
         String secondPair = s.nextLine();
 
-        int index1 = firstPair.indexOf(",");
+        int index1 = firstPair.indexOf(","); // The "integers" are being set to variables.
         int index2 = firstPair.indexOf(")");
         int indexX = secondPair.indexOf(",");
         int indexY = secondPair.indexOf(")");
@@ -19,18 +22,18 @@ public class EquationRunner {
         int y = indexX + 1;
 
 
-        int substring1x = Integer.parseInt(firstPair.substring(1,index1));
+        int substring1x = Integer.parseInt(firstPair.substring(1,index1)); // The "integers" are being parsed.
         int substring1y = Integer.parseInt(firstPair.substring(x,index2));
         int substring2x = Integer.parseInt(secondPair.substring(1,indexX));
         int substring2y = Integer.parseInt(secondPair.substring(y,indexY));
 
-        LinearEquation equation = new LinearEquation(substring1x, substring1y, substring2x, substring2y);
+        LinearEquation equation = new LinearEquation(substring1x, substring1y, substring2x, substring2y); // A object is being made.
         System.out.println(equation.toString());
 
-        System.out.println("Enter an x-value:");
+        System.out.println("Enter an x-value:"); // User is asked to enter a new x value to calculate coordinates based on previous equation.
         double x1 = s.nextDouble();
         double result = equation.solveEquation(x1);
-        System.out.println("Solved coordinate point is: (" + (double) x1 + ", " + Math.round(result*100.00)/100.00 + ")");
+        System.out.println("Solved coordinate point is: (" + df.format((double) x1) + ", " + df.format(Math.round(result*100.00)/100.00) + ")");
 
 
 
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,13 +1,16 @@
 import static java.lang.Math.*;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 public class LinearEquation {
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
-    private double x;
+    private int x1; // This variable represents the x coordinate.
+    private int y1; // This variable represents the y coordinate.
+    private int x2; // This variable represents the x2 coordinate.
+    private int y2; // This variable represents the y2 coordinate.
+    private double x; // This variable represents the x coordinate the user will input once the code is ran.
 
-    public LinearEquation(int x1, int y1, int x2, int y2)
+    DecimalFormat df = new DecimalFormat("0.00");
+    public LinearEquation(int x1, int y1, int x2, int y2) // This is a constructor to assign the coordinate values to the instance variables.
     {
         this.x1 = x1;
         this.x2 = x2;
@@ -18,14 +21,14 @@ public class LinearEquation {
     public int rise()
     {
         return y2 - y1;
-    }
+    } //  This method will return y2-y1.
 
     public int run()
     {
         return x2 - x1;
-    }
+    } // This method will return x2-x1.
 
-    public double slope()
+    public double slope() // This method will calculate the slope. (rise/run)
     {
         double rise = this.rise();
         double run = this.run();
@@ -33,19 +36,19 @@ public class LinearEquation {
         return slope;
     }
 
-    public double intercept()
+    public double intercept() // This method will return the y intercept of the line.
     {
         double intercept = (double) (y2 - (this.slope() * x2));
         return intercept;
     }
 
-    public String equation()
+    public String equation() // This method will return the linear equation of the two points.
     {
-        String linearEquation = "Slope Intercept Form: y = " + this.rise() + "/" + this.run() + "x + " + Math.round((this.intercept())*100.00)/(100.00);
+        String linearEquation = this.rise() + "/" + this.run() + "x + " + df.format(Math.round((this.intercept())*100.00)/(100.00));
         return linearEquation;
     }
 
-    public double distance()
+    public double distance() // This method will display the distance between the two points, using the equation sqrt((x2-x1)^2 + (y2-y1)^2)
     {
         double x = x2-x1;
         double y = y2-y1;
@@ -53,21 +56,21 @@ public class LinearEquation {
         return distance;
     }
 
-    public double solveEquation(double x)
+    public double solveEquation(double x) // This method will return the y coordinate of the linear equation when a specific x value is inputted.
     {
         this.x = x;
         double solve = ((double)this.slope())*(x) + this.intercept();
         return solve;
     }
 
-    public String toString()
+    public String toString() // This method will display and calculate all information about this instance, including x and y coordinates, slope intercept form equation, etc.
     {
         String firstPairInfo = "First Pair: (" + x1 + "," + y1 + ")";
         String secondPairInfo = "Second Pair: (" + x2 + "," + y2 + ")";
-        String slopeInfo = "Slope of Line: " + Math.round(this.slope()*100.00)/100.00;
-        String intercept = "Y-Intercept: " + Math.round(this.intercept()*100.00)/100.00;
+        String slopeInfo = "Slope of Line: " + df.format(Math.round(this.slope()*100.00)/100.00);
+        String intercept = "Y-Intercept: " + df.format(Math.round(this.intercept()*100.00)/100.00);
         String linearEquation = "Slope Intercept Form: " + this.equation();
-        String distance = "Distance Between Points: " + Math.round(this.distance()*100.00)/100.00;
+        String distance = "Distance Between Points: " + df.format(Math.round(this.distance()*100.00)/100.00);
         return firstPairInfo + "\n" + secondPairInfo + "\n" + slopeInfo + "\n" + intercept  + "\n" + linearEquation + "\n" + distance;
 
 
